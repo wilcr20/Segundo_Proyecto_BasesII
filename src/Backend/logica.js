7a0b678359e3505cf45c050805d3054d7c383581
Wilfred <wilcr20@gmail.com>
2018-10-06 15:34:04 -0600

    Realiza Conexion dBlink varias

--- src/Backend/logica.js
+++ src/Backend/logica.js
@@ -5,7 +5,6 @@ const { Pool, Client } = require('pg'); // Modulo requerido para definir
 
 exports.conectarServer = function conectarServer(data, callback) {
     //console.log("data.body recibido: ", data.body);
-
     client = new Client({
         user: data.body.username,
         host: data.body.server,
@@ -20,24 +19,66 @@ exports.conectarServer = function conectarServer(data, callback) {
             callback(false);
         } else {
             console.log("Conectado a base de dato");
+            creaDbLink();
             callback(true);
         }
     })
 }
 
 
-
-exports.conectarNodo = function conectarNodo(data, callback) {
-    client.query('SELECT * from persona', (err, res) => {
+creaDbLink = function() {
+    client.query("create extension dblink;", (err, res) => {
+        console.log("EROR: ", err);
         if (err) {
-            console.log("Error de conexion a datos.");
+            console.log("Error de dblink.");
+            // callback(false);
+
+        } else {
+            console.log("\n\nResultadoS: \n", res);
+            // callback(true);
+        }
+
+    }); // crea extension dblink
+
+}
+
+
+
+exports.conectarNodo = function conectarNodo(data, callback) { // realiza conexion dblink
+    //console.log("select dblink_connect( '" + data.body.conn + "', 'host=" + data.body.server + " user=" + data.body.username + " password=" + data.body.pasw + " dbname=" + data.body.database + "')");
+    client.query("select dblink_connect( '" + data.body.conn + "', 'host=" + data.body.server + " user=" + data.body.username + " password=" + data.body.pasw + " dbname=" + data.body.database + "')", (err, res) => {
+        if (err) {
+            console.log("Error de conexion a nodo.");
             callback(false);
 
         } else {
-            console.log("\n\nResultadoS: \n", err, res.rows);
+            console.log("\n\nConectado OK!: \n");
             callback(true);
         }
 
     })
 
 }
+
+
+
+
+/*
+exports.conectarNodo = function conectarNodo(data, callback) { // realiza conexion dblink recibiendo una tabla con varios atributos
+    console.log("...");
+
+    client.query("select * from dblink('host=" + data.body.server + " user=" + data.body.username + " password=" + data.body.pasw + " dbname=" + data.body.database + "', 'select schema_name from information_schema.schemata') as esquemas ( nombre  character varying );	", (err, res) => {
+        console.log("ERROR", err);
+        if (err) {
+            console.log("Error de conexion a datos.");
+            //callback(false);
+
+        } else {
+            console.log("\n\nResultadoS: \n", err, res.rows);
+            // callback(true);
+        }
+
+    })
+    console.log("..");
+
+}*/
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -21,24 +21,37 @@ export class HomeComponent {
    user:string ="postgres";
    pasw:string="alvarado";
    database:string='proyectoBases';
-   nuevoNodo:any;
+   queryUser:string= 'Select * from ';
+   numConexion=0;
+
+   nuevoNodo:any; // variable guarda objeto nodo creado
+
 
   pruebaNodo(){
     this.nuevoNodo = new nodo( this.ipServer,this.user,this.pasw, this.database );
-    this.listaNodo.push(this.nuevoNodo);
-    console.log("Se inserta nodo nuevo");
+
+    this.numConexion++;
 
     let jsonConect ={
       server:this.ipServer,
       username:this.user,
       pasw:this.pasw,
-      database:this.database
+      database:this.database,
+      conn:this.numConexion
     }
 
     return this.http.put("http://localhost:3000/conectarNodo",jsonConect)
     .subscribe(
       success => {
-        console.log("datos: ", success);
+        if(success == true){
+          swal('Correcto...', "Ingreso Exitoso.", 'success');
+          document.getElementById("closemodal").click();
+          this.listaNodo.push(this.nuevoNodo);
+          console.log("Se inserta nodo nuevo");
+        }else{
+          swal('Incorecto...', "Ingreso erroneo. Verifique sus credenciales.", 'error');
+        }
+        console.log("datos--->: ", success);
       },
       err => {
        swal('Incorrecto...', "Error de conexion con endpoint /conectarNodo.", 'error');
@@ -50,14 +63,19 @@ export class HomeComponent {
 
 
 
-  DBUsar(server,user,pasw,db){        /// aca estan las credenciales
+  DBUsar(server,user,pasw,db,dblink){        /// aca estan las credenciales
     console.log("Servidor: "+ server);
     console.log("User: "+ user);
     console.log("Pasw: "+ pasw);
     console.log("DataBase: "+ db);
-
-
-
+    console.log('dbLink: '+dblink);
   }
 
+  ConsultaQuery(server,user,pasw,db,dblink){
+    console.log("ejecuta consulta query");
+    console.log(" ---- "+ this.queryUser);
+  }
+
+
 }
+
